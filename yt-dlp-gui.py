8e94005a93faff871541812e41b3fb9cd847b57f
Komical <thatguy040503@gmail.com>
2025-11-17 01:12:56 -0500

    got progress bar to show up kinda

--- yt-dlp-gui.py
+++ yt-dlp-gui.py
@@ -1,10 +1,11 @@
 
 ####################################################
-# TODO: # Add dropdown for subtitles
-#       # Add dropdown for vid quality
-#       # Add dropdown for MP4 / MP3 / other
-#       # wf_submitURL functionality
-#       # Download bar
+# TODO: # Add functionality for subtitles
+#       # Add functionality for vid quality
+#       # Add functionality for MP4 / MP3 / other
+#       # Download bar but actually working, i think the gui and backend have to be seperate threads
+#       # Custom file path download with a browse
+#
 #       # if no FFMPEG downloaded, prompt user if they want to download
 #       # Multi download?
 #       # Multi threaded?
@@ -18,14 +19,14 @@ from tkinter import ttk
 import tkinter.messagebox
 
 def submit_url():
+
     url = wv_input_var.get()
     if not url:
         tkinter.messagebox.showerror("Error", "Enter a valid URL!")
         return
-    wv_message.set(f"Submitted: {url}")
 
-    # Call yt-dlp.exe with a URL
-    subprocess.run(["DLPGUI\yt-dlp.exe", url])
+    # calls and sets the msg in one line
+    wv_message.set(subprocess.getoutput(["DLPGUI\yt-dlp.exe", url]))
 
 def create_widgets(window):
     global wv_input_var, wv_message
@@ -37,11 +38,29 @@ def create_widgets(window):
 
     # url + submit widget
     wv_input_var = tk.StringVar()
-    w_urlEnter = tk.Entry(window, textvariable=wv_input_var, borderwidth=2, width = 60)
-    w_urlSubmit = ttk.Button(window, text="Submit", command=submit_url)
+    btn_urlEnter = tk.Entry(window, textvariable=wv_input_var, borderwidth=2, width = 60)
+    btn_urlSubmit = ttk.Button(window, text="Submit", command=submit_url)
+    btn_urlEnter.pack()
+    btn_urlSubmit.pack(pady=5)
 
-    w_urlEnter.pack()
-    w_urlSubmit.pack()
+    # Dropdown for file Type
+    fileTypes = ["Select a file type","MP4", "MP3"]
+    wv_ftoption = tk.StringVar(value="Select a file type")
+    btn_fileType = tk.OptionMenu(window, wv_ftoption, *fileTypes)
+    btn_fileType.pack(pady=5)
+
+    # Dropdown for video quality
+    quality = ["Select video quality","Best", "Worst"]
+    wv_option = tk.StringVar(value="Select video quality")
+    btn_fileType = tk.OptionMenu(window, wv_option, *quality)
+    btn_fileType.pack(pady=5)
+
+    # checkbox for subtitles
+    wv_subtitles = tk.BooleanVar(value=0)
+    btn_subtitles = tk.Checkbutton(window, variable=wv_subtitles, text="subtitles")
+    btn_subtitles.pack(pady=5)
+
+
 
 def main():
     #create window
