fbcd72b6d4e5db4bd77ca3c1d2d75adc0442ec32
Komical <thatguy040503@gmail.com>
2025-11-17 22:53:47 -0500

    idk

--- yt-dlp-gui.py
+++ yt-dlp-gui.py
@@ -21,69 +21,85 @@ import tkinter.messagebox
 
 def submit_url():
     command = create_command()
-    url = wv_input_var.get()
-    if not url:
-        wv_message.set("No URL Entered")
-        return
 
-    # calls and sets the msg 
-    result = subprocess.getoutput(command +" "+ url)
-    wv_message.set(result)
+    if command:
+        # calls and sets the msg 
+        result = subprocess.getoutput(command)
+        wv_message.set(result)
+
+    wv_input_var.set("")
+        
 
 
 # submit url helper
 def create_command():
     command = "yt-dlp.exe"
-
+    
+    #URL
+    url = wv_input_var.get()
+    if not url:
+        wv_message.set("No URL Entered")
+        return
+    
     # file type
     if wv_ftOption.get() != "Select a file type":
         command += " -t " + wv_ftOption.get().lower()
+    else:
+        wv_message.set("No file type selected")
+        return
 
     # quality
     if wv_qualOption.get() != "Select video quality":
         command += f' -S "res:{wv_qualOption.get()}"'
+    else:
+        wv_message.set("No Quality selected")
+        return
 
     # Subtitles
     if wv_subtitles.get():
         command += " --write-subs --sub-lang en"
 
-
+    command += " "+url
     print(command)
     return command
 
 
 def create_widgets(window):
-    global wv_message, wv_input_var, wv_ftOption, wv_qualOption, wv_subtitles
+    global wv_message, wv_input_var, wv_ftOption, wv_qualOption, wv_subtitles, btn_urlEnter
 
     # top message
     wv_message = tk.StringVar(value="Welcome")
-    message = tk.Label(window, textvariable=wv_message, bg="lightblue", height=3, width=50)
+    message = tk.Label(window, textvariable=wv_message, bg="lightblue", height=8, width=70)
     message.pack(pady=20)
 
     # url + submit widget
+    label_URL = tk.Label(window, text="URL:")
     wv_input_var = tk.StringVar()
-    btn_urlEnter = tk.Entry(window, textvariable=wv_input_var, borderwidth=2, width = 60)
+    btn_urlEnter = tk.Entry(window, textvariable=wv_input_var, borderwidth=2, width= 85)
     btn_urlSubmit = ttk.Button(window, text="Submit", command=submit_url)
-    btn_urlEnter.pack()
-    btn_urlSubmit.pack(pady=5)
-
+    
     # Dropdown for file Type
     fileTypes = ["Select a file type","MP4", "MP3"]
     wv_ftOption = tk.StringVar(value="Select a file type")
     btn_fileType = tk.OptionMenu(window, wv_ftOption, *fileTypes)
-    btn_fileType.pack(pady=5)
+    
 
     # Dropdown for video quality
-    quality = ["Select video quality","2160","1440","1920", "720", "480"]
+    quality = ["Select video quality","2160","1440","1920", "720", "480", "240", "144"]
     wv_qualOption = tk.StringVar(value="Select video quality")
-    btn_fileType = tk.OptionMenu(window, wv_qualOption, *quality)
-    btn_fileType.pack(pady=5)
+    btn_quality = tk.OptionMenu(window, wv_qualOption, *quality)
 
     # checkbox for subtitles
     wv_subtitles = tk.BooleanVar(value=0)
     btn_subtitles = tk.Checkbutton(window, variable=wv_subtitles, text="subtitles")
+    
+    # Packing
+    btn_fileType.pack(pady=5)
+    btn_quality.pack(pady=5)
     btn_subtitles.pack(pady=5)
-
+    label_URL.pack(anchor="sw", side="left", padx=5, pady=5)
+    btn_urlEnter.pack(anchor="sw", side="left", padx=5, pady=5)
+    btn_urlSubmit.pack(anchor = "sw", side = "right", padx=5, pady=5, fill="x")
 
 
 def main():
@@ -93,7 +109,7 @@ def main():
 
     #create window
     window = tk.Tk()
-    window.geometry("400x400")
+    window.geometry("640x320")
     window.resizable(width=False, height=False)
     window.title("YT-dlp-gui")
     window.config(background="gray")
